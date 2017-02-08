baf948de382b939aedf7bbca69d716272af5bf76
dmlyons2 <dmlyons2@gmail.com>
2017-02-08 14:53:37 -0500

    setup bootstrap and jquery

--- urls.py
+++ urls.py
@@ -1,13 +1,16 @@
 #  https://docs.djangoproject.com/en/1.6/intro/tutorial03/
 from django.conf import settings
-from django.conf.urls import include, url
-import views
+from django.conf.urls import url
+from . import views
+
+app_name = 'hem_app'
 
 if settings.IS_PUBLIC:
     urlpatterns = [
         #url(r'^api/', include('api.urls')),
         #url(r'^rest/', include('REST.urls')),
         url(r'^$', views.hem_landing_page),
+        url(r'^popgen/?$', views.hem_popgen),
         #url(r'^$', views.qed_splash_page_intranet),
         # url(r'^admin/', include(admin.site.urls)),
     ]
@@ -16,6 +19,7 @@ else:
         #url(r'^api/', include('api.urls')),
         #url(r'^rest/', include('REST.urls')),
         url(r'^$', views.hem_landing_page),
+        url(r'^popgen/?$', views.hem_popgen),
         #url(r'^$', views.qed_splash_page_intranet),
         # url(r'^admin/', include(admin.site.urls)),
     ]
--- views.py
+++ views.py
@@ -1,10 +1,13 @@
 from django.template.loader import render_to_string
-from django.http import HttpResponse
-from django.shortcuts import redirect
+from django.http import HttpResponse, HttpResponseRedirect
 #import links_left
-import os
+# import os
 #import secret
 from django.conf import settings
+from django.template.response import TemplateResponse
+from django.views.generic import View
+
+from django.shortcuts import get_object_or_404, render
 
 
 def hem_landing_page(request):
@@ -13,8 +16,10 @@ def hem_landing_page(request):
     html += render_to_string('02epa_drupal_header_bluestripe.html', {})
     html += render_to_string('03epa_drupal_section_title.html', {})
     if settings.IS_PUBLIC:
+        html += render_to_string('hem_index_public.html', {'title': 'Human Exposure Model'})
         pass
     else:
+        html += render_to_string('hem_index_internal.html', {'title': 'Human Exposure Model'})
         pass
     html += render_to_string('09epa_drupal_splashscripts.html', {})
     html += render_to_string('10epa_drupal_footer.html', {})
@@ -34,6 +39,13 @@ def file_not_found(request):
         html += render_to_string('04qed_splash_landing_intranet.html', {'title': 'qed'})
     html += render_to_string('09epa_drupal_splashscripts.html', {})
     html += render_to_string('10epa_drupal_footer.html', {})
+    response = HttpResponse()
+    response.write(html)
+    return response
+
+
+def hem_popgen(request):
+    html = render_to_string('hem_popgen.html', {})
     response = HttpResponse()
     response.write(html)
     return response
