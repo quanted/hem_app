7909a402bd5f543dadf4192ef17f87edaa9deaea
dmlyons2 <dmlyons2@gmail.com>
2017-03-02 16:43:06 -0500

    added relationship from category to products

--- models.py
+++ models.py
@@ -13,15 +13,21 @@ class RunHistory(models.Model):
     def __str__(self):
         return self.id
 
+    class Meta:
+        ordering = ('created_at',)
+
 
 class Category(models.Model):
     title = models.TextField(max_length=120)
-    description = models.TextField
+    description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     updated_at = models.DateTimeField(auto_now=True, editable=False)
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        ordering = ('title',)
 
 
 class Product(models.Model):
@@ -29,9 +35,12 @@ class Product(models.Model):
     product_type = models.TextField(max_length=75)
     product_type_refined = models.TextField(max_length=50)
     description = models.TextField()
-    category =  models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     updated_at = models.DateTimeField(auto_now=True, editable=False)
 
     def __str__(self):
         return self.product_type
+
+    class Meta:
+        ordering = ('product_type', 'product_type_refined',)
