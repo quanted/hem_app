822b6504421daad093d05d05928766396df9882a
dmlyons2 <dmlyons2@gmail.com>
2017-03-07 10:55:51 -0500

    ProductAssignment Model added

--- models.py
+++ models.py
@@ -30,11 +30,28 @@ class Category(models.Model):
         ordering = ('title',)
 
 
+class ProductAssignment(models.Model):
+    short_title = models.TextField(max_length=2)
+    title = models.TextField(max_length=75)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, editable=False)
+
+    def __str__(self):
+        return self.short_title + ', ' + self.title
+
+    class Meta:
+        ordering = ('title',)
+
+
 class Product(models.Model):
+    sheds_id = models.TextField(default='', max_length=50)
+    sheds_product_category_id = models.TextField(default='', max_length=50)
     puc_id = models.TextField(editable=False)
+    group = models.TextField(max_length=75, default='other')
     product_type = models.TextField(max_length=75)
     product_type_refined = models.TextField(max_length=50)
     description = models.TextField()
+    assignment = models.ForeignKey(ProductAssignment, on_delete=models.CASCADE, default=1)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     updated_at = models.DateTimeField(auto_now=True, editable=False)
