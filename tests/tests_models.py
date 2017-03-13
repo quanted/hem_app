dbe3db2ee30b1f4d8b80e4a57d8be245e49d407b
dmlyons2 <dmlyons2@gmail.com>
2017-03-13 09:35:17 -0400

    Unit tests set up and working

--- tests/tests_models.py
+++ tests/tests_models.py
@@ -1,6 +1,39 @@
+from factories import *
 from django.test import TestCase
-from hem_app.models import Product
+
 
 class ProductTestCase(TestCase):
-    def setUp(self):
-        
+
+
+    def test_string_representation(self):
+        product = ProductFactory()
+        self.assertEquals(str(product), product.product_type)
+
+
+class CategoryTestCase(TestCase):
+
+    def test_string_representation(self):
+        category = CategoryFactory()
+        self.assertEquals(str(category), category.title)
+
+
+class ProductAssignmentTestCase(TestCase):
+
+    def test_string_representation(self):
+        productassignment = ProductAssignmentFactory()
+        x = productassignment.short_title + ', ' + productassignment.title
+        self.assertEquals(str(productassignment), x)
+
+
+class RunHistoryTestCase(TestCase):
+
+    def test_string_representation(self):
+        runhistory = RunHistoryFactory()
+        self.assertEquals(str(runhistory), str(runhistory.id))
+
+
+class RunParamsTestCase(TestCase):
+
+    def test_string_representation(self):
+        runparams = RunParamsFactory()
+        self.assertEquals(str(runparams), str(runparams.population_size))
