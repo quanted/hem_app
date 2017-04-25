3cc0c34c71c82e14fc6305debcbf48011ca9149a
GBhander <bhander.gurbakhash@epa.gov>
2017-04-25 08:17:37 -0400

    Fixed updated java script file, updated popgen and results files, added css.

--- views.py
+++ views.py
@@ -1,14 +1,16 @@
 from django.template.loader import render_to_string
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, Http404, JsonResponse
 from django.shortcuts import render, render_to_response
 #import links_left
-import os, tempfile, zipfile
+import os, tempfile, zipfile, json
 #import secret
 from django.conf import settings
 from wsgiref.util import FileWrapper
 import mimetypes
-from .forms import HemForm
-from .models import Category
+from .forms import HemForm, popgenForm
+from .models import Category, RunHistory
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 
 def send_file(request):
@@ -59,23 +61,77 @@ def file_not_found(request):
 
 def hem_popgen(request):
     """ Main landing and form for hem_app """
-    all_categories = Category.objects.all().values_list('id', 'title')
+    all_categories = Category.objects.filter(parent=None).values_list('id', 'title')
+    form = popgenForm(request.POST)
     if request.method == 'POST':
-        form = HemForm(request.POST)
         if form.is_valid():
-            form.save(commit=True)
-        return HttpResponseRedirect('results')
+            sub_category = request.POST.get('sub_category', -1)
+            category = Category.objects.get(id=sub_category) #form.cleaned_data['sub_category1'])
+            product= request.POST.get('toggleProducts', True)
+            population_size = form.cleaned_data.get('population_field')
+            gender = request.POST['optionsRadiosGender']
+            age_radio = request.POST['optionsRadiosAge']
+            min_age = 0
+            mix_age = 0
+            if age_radio == 'age1':
+                min_age = 0
+                mix_age = 5
+            elif age_radio == 'age2':
+                min_age = 6
+                mix_age = 12
+            elif age_radio == 'age3':
+                min_age = 13
+                mix_age = 15
+            elif age_radio == 'age4':
+                min_age = 16
+                mix_age = 18
+            elif age_radio == 'age5':
+                min_age = 19
+                mix_age = 49
+            elif age_radio == 'age6':
+                min_age = 49
+                mix_age = 99
+            history = RunHistory(categories=category, products=product, population_size=population_size,
+                                 gender=gender, min_age=min_age, max_age=mix_age)
+            history.save()
+            return HttpResponseRedirect('results',  { 'all_historyRows': all_categories })
+        else: raise Http404('Unable to save population generation data..')
     else:
-        form = HemForm()
-
-        return render(request,
-                      'hem_popgen.html',
-                      {'form': form, 'all_categories': all_categories})
+        form = popgenForm()
+        return render(request, 'hem_popgen.html', {'form': form, 'all_categories': all_categories })
 
 
 def hem_results(request):
     """ Landing page for results of model run """
-    html = render_to_string('hem_results.html', {})
+    all_history = RunHistory.objects.all()
+    html = render_to_string('hem_results.html', {'all_history':all_history })
     response = HttpResponse()
     response.write(html)
     return response
+
+
+def get_json_data(request):
+    #data1 = [data.gender and data.population_size  for data in RunHistory.objects.all()]
+    data = dict(history=list(RunHistory.objects.values('gender', 'population_size', 'min_age', 'max_age', 'categories_id')))
+    return JsonResponse(data, safe=True)
+
+
+def query_category(request):
+    parent_id = request.GET.get('care_id', None)
+    data = list(Category.objects.filter(parent_id=parent_id).values('id', 'title'))  #.values_list('id', 'title'))
+    return JsonResponse({'care_id' : data }, content_type="application/json", safe=True)
+
+
+#class get_results(APIView):
+#    authentication_classes = ()
+#    permission_classes = ()
+
+#    def get(self, request, format=None):
+#        gender = [data.gender for data in RunHistory.objects.all()]
+#        population_size = [data.population_size for data in RunHistory.objects.all()]
+#        data = {
+#            'Gender': gender,
+#            'Popupation': population_size
+#        }
+#        return Response(data)
+
